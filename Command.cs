257e3c67f3753d35ad27d896921933ec58f98c2b
Ruohan Wang <Ruohan.Wang@aecom.com>
2022-11-08 23:05:01 -0700

    add function to create new line style

--- Command.cs
+++ Command.cs
@@ -25,11 +25,20 @@ namespace Change_Line_Type
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
+            // declare correct line style formate
+            string lineStyleNamingConvention = "STM-EP";
+
+            /*
+            // retrive all detail lines and their line styles
             IEnumerable<CurveElement> detailLines = (IEnumerable<CurveElement>)FindAllDetailCurves(doc).Item1;
             IEnumerable<string> detailLineStyles = (IEnumerable<string>)FindAllDetailCurves(doc).Item2;
 
-            IEnumerable<CurveElement> targetedDetailCurve = FindDetailLinesWithIncorrectLineStyle(detailLines, detailLineStyles);
+            // filter through all detail lines to retrive the ones with incorrect line style
+            IEnumerable<CurveElement> targetedDetailCurve = FindDetailLinesWithIncorrectLineStyle(detailLines, detailLineStyles, lineStyleNamingConvention);
+            */
 
+            // get all avaliable graphic styles for detail lines that follow the naming convention
+            IEnumerable<GraphicsStyle> existingTargetedGrpahicStyle = getAllCorrectGraphicStyle(doc, lineStyleNamingConvention);
 
             return Result.Succeeded;
         }
@@ -78,19 +87,21 @@ namespace Change_Line_Type
         }
 
         // filter the line in the incorrect LineStyle
-        private static IEnumerable<CurveElement> FindDetailLinesWithIncorrectLineStyle(IEnumerable<CurveElement> curves, IEnumerable<string> lineStyles)
+        private static IEnumerable<CurveElement> FindDetailLinesWithIncorrectLineStyle(IEnumerable<CurveElement> curves, IEnumerable<string> lineStyles, string lineStyleName)
         {
             // declare detailCurves with incorrect LineStyle
             IList<CurveElement> incorrectedCurve = new List<CurveElement>();
+
             int count = 0;
 
-            var cS = curves.Zip(lineStyles, (x, y) => new Tuple<CurveElement, string>(x, y))
-                            .ToList();
+            var cS = curves.Zip(lineStyles, (x, y) => new Tuple<CurveElement, string>(x, y)).ToList();
 
-            foreach (var c in cS) 
+            foreach (var c in cS)
             {
                 string cStyle = c.Item2;
-                if (!cStyle.StartsWith("STM"))
+                bool incorrect = !cStyle.StartsWith(lineStyleName) | !cStyle.StartsWith("<");
+
+                if (incorrect)
                 {
                     count++;
                     incorrectedCurve.Add(c.Item1);
@@ -131,10 +142,67 @@ namespace Change_Line_Type
             return targetedDetailCurve;
         }
 
+        // collect all graphic styles that follows' standard. 
+        private static IEnumerable<GraphicsStyle> getAllCorrectGraphicStyle(Document doc, string lineStyleName)
+        {
+            IEnumerable<GraphicsStyle> targetedGraphicStyles = new FilteredElementCollector(doc)
+                .OfClass(typeof(GraphicsStyle))
+                .Cast<GraphicsStyle>()
+                .Where(gs => gs.GraphicsStyleCategory.ToString().Contains(lineStyleName));
+
+            // report result with task dialog
+            if (targetedGraphicStyles != null)
+            {
+                string graphicsStyleNamesConcatenate = string.Join(", ", targetedGraphicStyles);
+
+                TaskDialog td = new TaskDialog("Success")
+                {
+                    Title = "avaliable graphic styles",
+                    AllowCancellation = true,
+                    MainContent = "found avaliable grphic styles",
+                    MainInstruction = $"{targetedGraphicStyles.Count()} availible graphic styles ({graphicsStyleNamesConcatenate}) of detail lines that follow the naming convention",
+                    MainIcon = TaskDialogIcon.TaskDialogIconInformation,
+                };
+                td.CommonButtons = TaskDialogCommonButtons.Ok;
+                td.Show();
+            }
+            else 
+            { 
+
+            }
+
+            return targetedGraphicStyles;
+        }
+
+        // create line style following the nameing convention
+        private static void CreateLineStyle(Document doc, string detailLineName,IList<byte> c, int detailLineWeight) 
+        {
+            // retrive categories and BuiltIn category for lines from the document settings
+            Settings settings = doc.Settings;
+            Categories cats = settings.Categories;
+            Category lineCat = cats.get_Item(BuiltInCategory.OST_Lines);
+
+            // create new line style with the NewSubcategory method
+            Category lineStyleCat = cats.NewSubcategory(lineCat, detailLineName);
+
+            // set the line weight and line color for the new line weight.
+            lineCat.LineColor = new Color(c[0], c[1], c[2]);
+            lineCat.SetLineWeight(detailLineWeight, GraphicsStyleType.Projection);
+        }
+
+        /*
         // select lines whose name is not following the project standard by filtering the name of LineType.
-        // get the Style of all detail lines, 
-        // get the Line pattern of all detail lines
-        // get the Line color of all detail lines
+        
+        private static (string, string, string) compareCurveAttribute(IEnumerable<CurveElement> curve)
+        {
+            // get the Style of all detail lines, 
+            // get the Line pattern of all detail lines
+            // get the Line color of all detail lines
+
+
+        };
+
+        */
 
         // change the the line types of selected lines
 
